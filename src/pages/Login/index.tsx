d23017bdbfb1f8c1d1d7d9edb77da9fadb062856
Marco Segalla <marco.segalla@sorteonline.com.br>
2022-11-16 19:41:45 -0300

    Criando página de login e formulário

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import React, { useRef } from 'react'
+import { useForm } from 'react-hook-form'
+
+import loginImage from '../../assets/login-image.webp'
+import Input from '../../components/Input'
+import { loginFormValidation } from './loginFormValidation'
+import { Container, FormContainer, Image, Form, ButtonBase, Button } from './styles'
+
+interface LoginFormData {
+  email: string
+  password: string
+}
 
 const Login: React.FC = () => {
-  return <div>Login</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: zodResolver(loginFormValidation),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const inputRef = useRef(null)
+
+  const handleLoginSubmit = (data: LoginFormData) => {
+    console.log(data)
+    reset()
+  }
+
+  return (
+    <Container>
+      <FormContainer>
+        <Image src={loginImage} />
+        <Form onSubmit={handleSubmit(handleLoginSubmit)}>
+          <Input {...inputRef} label="Email" type="email" placeholder="Digite seu email" {...register('email')} />
+          <Input {...inputRef} label="Senha" type="password" placeholder="Digite sua senha" {...register('password')} />
+          <ButtonBase>
+            <Button type="submit">Entrar</Button>
+          </ButtonBase>
+        </Form>
+      </FormContainer>
+    </Container>
+  )
 }
 
 export default Login
